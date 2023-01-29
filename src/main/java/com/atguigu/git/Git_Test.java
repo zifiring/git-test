ca17662c0bfb29d37b2dc0f6ded8e6b0b617d6c4
yuanyin <zhaofayinghi@163.com>
2023-01-29 15:24:24 +0800

    second commit

--- src/main/java/com/atguigu/git/Git_Test.java
+++ src/main/java/com/atguigu/git/Git_Test.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Git_Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
