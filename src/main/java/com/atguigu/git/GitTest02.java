5494f0528982d5081a3ef57719153a460511a65d
yuanyin <zhaofayinghi@163.com>
2023-02-05 23:20:45 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest02.java
+++ src/main/java/com/atguigu/git/GitTest02.java
@@ -6,5 +6,6 @@ public class GitTest02 {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
