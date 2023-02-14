80e3a3bfd6da268bc3a82236ef9f63e381eae9c4
yuanyin <zhaofayinghi@163.com>
2023-02-14 13:05:29 +0800

    gitee commit

--- src/main/java/com/atguigu/git/GitTest02.java
+++ src/main/java/com/atguigu/git/GitTest02.java
@@ -7,5 +7,6 @@ public class GitTest02 {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("gitee test!");
     }
 }
